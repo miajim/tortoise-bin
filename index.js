e8389445a8346caf932c7e5da57f885070346527
Charles A. Hartman <charles@hartman.dev>
2022-02-04 08:30:54 -0800

    fixed sort of requests, also more cleaning and refactoring

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const express = require('express')
+const express = require('express');
 const favicon = require('serve-favicon');
 const path = require('path');
-const bodyParser = require('body-parser') // middleware that parses incoming requests
-const hbs = require('hbs')                // handlebars
+const bodyParser = require('body-parser'); // middleware that parses incoming requests
+// const hbs = require('hbs');             // handlebars (this require not needed)
 
-const app = express()
+const app = express();
 
-const { pool } = require('./config')      // Enables connection to postgres
-const cors = require('cors')              // Helps us avoid cors issues
-const port = 3005
+const { pool } = require('./config');      // Enables connection to postgres
+const cors = require('cors');              // Helps us avoid cors issues
+const port = 3005;
 
 app.set('view engine', 'hbs');            // handlebars
 
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.png')));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-)
+);
 
-app.use(cors())
+app.use(cors());
 
 function binPath(len) {
   return [...Array(len)]
     .map(() => Math.random()
-    .toString(36)[2])
+      .toString(36)[2])
     .join('')
     .toUpperCase();
 }
 
 app.get('/', (request, response) => {
-  response.render('index')
-})
+  response.render('index');
+});
 
 // Creation of new bin
 app.post('/createBin', (request, response) => {
@@ -41,48 +41,50 @@ app.post('/createBin', (request, response) => {
   const timestamp = Date.now();
   const queryString = `INSERT INTO bins (bin_path, creation_time) VALUES ($1, (to_timestamp(${timestamp} / 1000.0)))`;
 
-  pool.query(queryString, [newBinPath], (error, results) => {
+  pool.query(queryString, [newBinPath], (error) => {
     if (error) {
-      throw error
+      throw error;
     }
 
     response.status(308).redirect(`/r/${newBinPath}/all`);
   });
-})
+});
 
 // This is the endpoint for the webhook
 app.post('/r/:bin_path', (request, response) => {
   const binPath = request.params.bin_path;
-  const queryExistsString = `SELECT bin_id, bin_path FROM bins WHERE bin_path='${binPath}'`
+  const queryExistsString = `SELECT bin_id, bin_path FROM bins WHERE bin_path='${binPath}'`;
 
   pool.query(queryExistsString, (error, results) => {
     console.log(results.rows);
     if (results.rows.length === 0) {
-      response.status(404).json({status: "fail", message: "bin does not exist"})
+      response.status(404).json({status: "fail", message: "bin does not exist"});
     } else {
-      
-      binID = results.rows[0].bin_id;
+
+      let binID = results.rows[0].bin_id;
 
       console.log(binID);
 
       const headers = JSON.stringify(request.headers);
       const timestamp = Date.now();
       const payload = request.body;
-      // const originIp = request.ip;
+      const originIp = request.ip;
+
+      console.log(originIp);
 
       const queryInsertString = `INSERT INTO requests (headers, request_payload, bin_id, time_received, request_type) 
       VALUES ($1, $2, $3, (to_timestamp(${timestamp} / 1000.0)), 'POST')`;
-      
-      pool.query(queryInsertString, [headers, payload, binID], (error, results) => {
+
+      pool.query(queryInsertString, [headers, payload, binID], (error) => {
         if (error) {
-          throw error
+          throw error;
         }
-    
-        response.status(201).json({ status: "success", message: "New request added" })
+
+        response.status(201).json({ status: "success", message: "New request added" });
       });
     }
   });
-})
+});
 
 app.get('/r/:bin_path/all', (request, response) => {
   console.log(request.params);
@@ -93,27 +95,27 @@ app.get('/r/:bin_path/all', (request, response) => {
 
   const queryString = `SELECT request_type, request_origin_ip, time_received, headers, request_payload FROM requests
   JOIN bins ON requests.bin_id = bins.bin_id
-  WHERE bins.bin_path = '${binPath}';`
+  WHERE bins.bin_path = '${binPath}' ORDER BY time_received DESC;`;
 
   pool.query(queryString, (error, results) => {
     const queryResults = JSON.stringify(results.rows, null, 2);
-    const logs = { binPath: fullURL, queryResults: queryResults, }
+    const logs = { binPath: fullURL, queryResults: queryResults, };
     response.render('binRequest', logs);
     // response.status(200).json(results.rows);
   });
-})
+});
 
 // Test route just to make sure we can query the DB Charles made
 app.get('/bins', (request, response) => {
   pool.query('SELECT * FROM bins', (error, results) => {
     if (error) {
-      throw error
+      throw error;
     }
 
-    response.status(200).json(results.rows)
-  })
-})
+    response.status(200).json(results.rows);
+  });
+});
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+  console.log(`App running on port ${port}.`);
+});
